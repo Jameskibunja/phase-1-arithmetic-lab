372d63959834bfbae8867d70b15f87607d14ea97
JAMES <jameskibunja17@gmail.com>
2022-12-04 14:23:04 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 12;
+const num4 = 3;
 
-// Write your code here
+let multiply = num1 * num2;
+var random = Math.floor(Math.random() * 10);
+var mod = num3/num4;
+var max = Math.max(1,3,20,10,9,4);
+
+
